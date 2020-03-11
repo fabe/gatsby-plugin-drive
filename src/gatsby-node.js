0492237daa77e061ebf52a7f7c83a7dfd4f1bb14
Max Steenbergen <maxsteenbergen@gmail.com>
2020-03-11 21:36:13 +0000

    Support exporting Google Sheets to XLSX
    We have a folder of both Google Docs and Google Sheets. This will allow us to grab them both.

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -6,28 +6,26 @@ const fs = require(`fs`);
 const log = str => console.log(`\n\n🚗 `, str);
 const FOLDER = `application/vnd.google-apps.folder`;
 const GOOGLE_DOC = 'application/vnd.google-apps.document';
+const GOOGLE_SHEET = 'application/vnd.google-apps.spreadsheet';
 
 let shouldExportGDocs;
-let exportMime;
+let shouldExportGSheets;
+let exportDocMime;
+let exportSheetMime;
 let middleware;
 
-exports.onPreBootstrap = (
-  { graphql, actions },
-  { folderId, keyFile, key, destination, exportGDocs, exportMimeType, exportMiddleware }
-) => {
+exports.onPreBootstrap = ({ graphql, actions }, { folderId, keyFile, key, destination, exportGDocs, exportDocMimeType, exportGSheets, exportSheetMimeType, exportMiddleware }) => {
   return new Promise(async resolve => {
     log(`Started downloading content`);
 
     // Get token and fetch root folder.
-    const token = keyFile ? 
-      await googleapi.getToken({ keyFile }) :
-      await googleapi.getToken({ key });
+    const token = keyFile ? await googleapi.getToken({ keyFile }) : await googleapi.getToken({ key });
     const cmsFiles = await googleapi.getFolder(folderId, token);
     shouldExportGDocs = exportGDocs;
-    exportMime = exportMimeType;
-    middleware = exportMiddleware === undefined 
-      ? x => x 
-      : exportMiddleware; 
+    shouldExportGSheets = exportGSheets;
+    exportDocMime = exportDocMimeType;
+    exportSheetMime = exportSheetMimeType;
+    middleware = exportMiddleware === undefined ? x => x : exportMiddleware;
 
     // Create content directory if it doesn't exist.
     mkdirp(destination);
@@ -44,9 +42,7 @@ exports.onPreBootstrap = (
 function recursiveFolders(array, parent = '', token, destination) {
   return new Promise(async (resolve, reject) => {
     let promises = [];
-    let filesToDownload = shouldExportGDocs
-      ? array
-      : array.filter(file => file.mimeType !== GOOGLE_DOC)
+    let filesToDownload = shouldExportGDocs || shouldExportGSheets ? array : array.filter(file => (file.mimeType !== GOOGLE_DOC) && (file.mimeType !== GOOGLE_SHEET));
 
     for (let file of filesToDownload) {
       // Check if it`s a folder or a file
@@ -57,33 +53,37 @@ function recursiveFolders(array, parent = '', token, destination) {
 
         // Then, get the files inside and run the function again.
         const files = await googleapi.getFolder(file.id, token);
-        promises.push(
-          recursiveFolders(files, `${parent}/${file.name}`, token, destination)
-        );
+        promises.push(recursiveFolders(files, `${parent}/${file.name}`, token, destination));
       } else {
-        promises.push(
-          new Promise(async (resolve, reject) => {
-            // If it`s a file, download it and convert to buffer.
-            const dest = path.join(destination, parent, getFilenameByMime(file));
+        promises.push(new Promise(async (resolve, reject) => {
+          // If it`s a file, download it and convert to buffer.
+          const dest = path.join(destination, parent, getFilenameByMime(file));
 
-            if (fs.existsSync(dest)) {
-              resolve(getFilenameByMime(file));
-              return log(`Using cached ${getFilenameByMime(file)}`);
-            }
+          if (fs.existsSync(dest)) {
+            resolve(getFilenameByMime(file));
+            return log(`Using cached ${getFilenameByMime(file)}`);
+          }
+          
+          let buffer;
+          switch (file.mimeType) {
+            case (GOOGLE_DOC):
+              buffer = await middleware(googleapi.getGDoc(file.id, token, exportDocMime))
+              break;
+            case (GOOGLE_SHEET):
+              buffer = await middleware(googleapi.getGDoc(file.id, token, exportSheetMime))
+              break;
+            default:
+              buffer = await googleapi.getFile(file.id, token)
+          }
 
-            const buffer = file.mimeType === GOOGLE_DOC
-              ? await middleware(googleapi.getGDoc(file.id, token, exportMime))
-              : await googleapi.getFile(file.id, token);
+          // Finally, write buffer to file.
+          fs.writeFile(dest, buffer, err => {
+            if (err) return log(err);
 
-            // Finally, write buffer to file.
-            fs.writeFile(dest, buffer, err => {
-              if (err) return log(err);
-
-              log(`Saved file ${getFilenameByMime(file)}`);
-              resolve(getFilenameByMime(file));
-            });
-          })
-        );
+            log(`Saved file ${getFilenameByMime(file)}`);
+            resolve(getFilenameByMime(file));
+          });
+        }));
       }
     }
 
@@ -91,21 +91,15 @@ function recursiveFolders(array, parent = '', token, destination) {
   });
 }
 
-const fileExtensionsByMime = new Map([
-  ['text/html', '.html'],
-  ['application/zip', '.zip'],
-  ['text/plain', '.txt'],
-  ['application/rtf', '.rtf'],
-  ['application/vnd.oasis.opendocument.text', '.odt'],
-  ['application/pdf', '.pdf'],
-  ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', '.docx'],
-  ['application/epub+zip', '.epub']
-]);
+const fileExtensionsByMime = new Map([['text/html', '.html'], ['application/zip', '.zip'], ['text/plain', '.txt'], ['application/rtf', '.rtf'], ['application/vnd.oasis.opendocument.text', '.odt'], ['application/pdf', '.pdf'], ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', '.docx'], ['application/epub+zip', '.epub'], ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', '.xlsx']]);
 
 const getFilenameByMime = file => {
   if (file.mimeType === GOOGLE_DOC) {
-    return `${file.name}${fileExtensionsByMime.get(exportMime)}`
+    return `${file.name}${fileExtensionsByMime.get(exportDocMime)}`;
+  }
+  else if (file.mimeType === GOOGLE_SHEET) {
+    return `${file.name}${fileExtensionsByMime.get(exportSheetMime)}`;
   } else {
     return file.name;
   }
-}
+};
